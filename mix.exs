b35eb7f2236d06d91f5a6bd68117f0d6756ba24a
Rohan Pujari <rohan.pujari@kiprosh.com>
2015-12-26 12:01:53 +0530

    Added ex_doc dev dependincy for generating documentation

--- mix.exs
+++ mix.exs
@@ -35,7 +35,9 @@ defmodule ChhutiServer.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.11", only: :dev}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
